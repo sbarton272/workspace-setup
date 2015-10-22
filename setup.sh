5a42264a4ca4944434b0734be27c6c5a77a1d157
Spencer Barton <sebarton@nestlabs.com>
2015-10-21 22:35:45 -0700

    Updated script to move files

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Install plugins
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
+# Move files to root dir
+cp vimrc ~/.vimrc
+cp -r vim/ ~/.vim/
 
