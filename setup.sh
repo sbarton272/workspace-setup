f67998a9d3c7a18ab7edb07f847d2d380b7dd24f
Spencer Barton <sbarton272@gmail.com>
2020-04-21 11:20:06 -0700

    Add zsh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 ./setup_vim.sh
 ./setup_bash.sh
 ./setup_git.sh
-
+./setup_pip.sh
---
+++ setup_zsh.sh
@@ -0,0 +1,4 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+upgrade_oh_my_zsh
+
+cp zsh/* ~/
